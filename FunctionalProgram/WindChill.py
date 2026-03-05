3587683661a1ede6086e3e53e8235bbbfaa68cb4
Ayush Agrawal <agrawal07ayushbansal@gmail.com>
2026-03-05 11:48:01 +0530

    Add some condition to validate the answer

--- FunctionalProgram/WindChill.py
+++ FunctionalProgram/WindChill.py
@@ -6,6 +6,8 @@ c=float(input("Enter the value of y:"))
 def clacu_windchill(t,v):
     return 35.75 +0.6215*t+(0.4275-37.75)*math.pow(v,0.16)
 
+if(t>50 and (c>150 or c<3)):
+    print("Invalid value")
 wind=clacu_windchill(t,c)
 print(wind)
 
